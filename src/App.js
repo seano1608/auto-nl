044bd72444fc2f57b078949099ceaf3529aacaae
Sean Thomson <seantomo1680@gmail.com>
2024-06-25 21:19:20 +0200

    `added` price sort

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ const App = () => {
     const [cars, setCars] = useState([]);
     const [filteredCars, setFilteredCars] = useState([]);
     const [isFiltered, setIsFiltered] = useState(false);
+    const [sortOrder, setSortOrder] = useState('asc');
 
     useEffect(() => {
         const getCars = async () => {
             try {
                 const data = await fetchCars('toyota', 0, 15);
                 setCars(data);
+                setFilteredCars(data);
             } catch (error) {
                 console.error(error);
             }
@@ -35,6 +37,29 @@ const App = () => {
         setIsFiltered(!isFiltered);
     };
 
+    const handleSortOrderChange = () => {
+        const updatedSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+        setSortOrder(updatedSortOrder);
+        applyFilters(filteredCars, updatedSortOrder);
+    };
+
+    const applyFilters = (carplay, sort) => {
+        let filtered = cars;
+
+        if (carplay) {
+            filtered = filtered.filter(({ accessories }) =>
+                accessories.some(({ items }) =>
+                    items.includes('Apple Carplay/Android Auto')
+                )
+            );
+        }
+
+        // Sort by price
+        filtered.sort((a, b) => sort === 'asc' ? a.price - b.price : b.price - a.price);
+
+        setFilteredCars(filtered);
+    };
+
   return (
       <div className="App">
           <div className="container mx-auto p-4">
@@ -44,6 +69,12 @@ const App = () => {
                   className="mb-4 px-4 py-2 bg-blue-500 text-white rounded"
               >
                   {isFiltered ? 'Show All Cars' : 'Filter by Apple CarPlay/Android Auto'}
+              </button>
+              <button
+                  onClick={handleSortOrderChange}
+                  className="px-4 py-2 bg-blue-500 text-white rounded"
+              >
+                  {sortOrder === 'asc' ? 'Sort by Price: High to Low' : 'Sort by Price: Low to High'}
               </button>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                   {filteredCars.map(car => (
